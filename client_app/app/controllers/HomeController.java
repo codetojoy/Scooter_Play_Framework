93d75cfb817b5cfa3511ca202bbc119aa42ecb3b
Michael Easter <codetojoy@gmail.com>
2023-04-02 14:21:01 -0300

    basic retrieval from server

--- client_app/app/controllers/HomeController.java
+++ client_app/app/controllers/HomeController.java
@@ -1,15 +1,51 @@
 package controllers;
 
 import play.mvc.*;
+import play.libs.ws.*;
+
+import javax.inject.Inject;
+import java.util.*;
+import java.util.concurrent.*;
+
+import com.fasterxml.jackson.databind.JsonNode;
 
 /**
  * This controller contains an action to handle HTTP requests
  * to the application's home page.
  */
-public class HomeController extends Controller {
+public class HomeController extends Controller implements WSBodyReadables {
+
+    private final WSClient ws;
+    private static final String ATTENDEES_ENDPOINT = "http://localhost:9000/v1/attendees";
+
+    @Inject
+    public HomeController(WSClient ws) {
+      this.ws = ws;
+    }
 
     public Result index() {
         return ok(views.html.index.render());
     }
 
+    public CompletionStage<Result> go() {
+        return ws.url(ATTENDEES_ENDPOINT)
+            .get()
+            .thenApply(response -> {
+                System.out.println("TRACER api call OK cp 0");
+                var json = response.asJson();
+                var jsonStr = json.toPrettyString();
+                System.out.println("TRACER api call OK cp 1");
+                return ok(views.html.attendees.render(jsonStr));
+            });
+    }
+    /*
+     *
+    public CompletionStage<Result> go() {
+        // implements WSBodyReadables or use WSBodyReadables.instance.json()
+        return ws.url(url).get().thenApply(r -> {
+            r.getBody(json());
+            return ok(views.html.attendees.render());
+        });
+    }
+     */
 }
